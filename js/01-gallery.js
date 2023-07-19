1958d4207d5c18f0e1859186a74d3ddf6d90a1b9
Horbunov Yurii <urijnet@gmail.com>
2023-07-19 16:52:47 +0300

    start

---
+++ js/01-gallery.js
@@ -0,0 +1,9 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.dir(galleryItems);
+
+
+const listRef = document.querySelector("gallery");
+
+const marcup = galleryItems
