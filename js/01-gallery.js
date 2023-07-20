d420e77a9d356899542d07e6c17d465c8650f68b
Horbunov Yurii <urijnet@gmail.com>
2023-07-20 12:21:08 +0300

    fix-tasck-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,9 +1,62 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.dir(galleryItems);
 
 
-const listRef = document.querySelector("gallery");
+const listRef = document.querySelector(".gallery");
+
 
 const marcup = galleryItems
+  .map(
+    ({ description, preview, original }) =>
+      `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+  )
+  .join("");
+
+ 
+
+listRef.insertAdjacentHTML("beforeend", marcup);
+
+
+
+listRef.addEventListener('click', onClick);
+window.addEventListener("keydown", onEscapeClick);
+
+let instance;
+
+function onClick(evt){
+  evt.preventDefault();
+
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const imgEl = evt.target.dataset.source;
+
+     instance = basicLightbox.create(`
+      <img src="${imgEl}" width="800" height="600">
+  `);
+
+  instance.show()
+
+};
+
+
+function onEscapeClick(evt) {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+}
+
+
+
+
